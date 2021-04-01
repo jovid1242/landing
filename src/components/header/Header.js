6726018abbd724721716a6fa7dbecb02f7f2dde8
jovid1242 <jovid1242@gmail.com>
2021-04-01 20:18:57 +0500

    update header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,10 +6,19 @@ import Navigation from './components/navigation/Navigation'
 import './Header.css'
 
 export default function Header() {
+    window.onscroll = function (e) {
+        const nav = document.querySelector('.navbar')
+        if (window.scrollY > 0) {
+            nav.classList.add('affix');
+        }
+        if (window.scrollY === 0) {
+            nav.classList.remove('affix')
+        }
+    };
     return (
         <>
             <div className="header">
-                <nav className="navbar navbar-expand-lg fixed-top bg-transparent ">
+                <nav className="navbar navbar-expand-lg fixed-top bg-transparent">
                     <div className="container">
                         <Logo />
                         <Menu />
