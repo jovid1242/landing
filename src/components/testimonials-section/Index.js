26c7282f0d006b0a0e5161849e02a8013adbae71
jovid1242 <jovid1242@gmail.com>
2021-04-01 23:52:17 +0500

    fix

--- src/components/testimonials-section/Index.js
+++ src/components/testimonials-section/Index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import img1 from '../../assets/img/img1.jpg'
-import img2 from '../../assets/img/img2.jpg'
-import img3 from '../../assets/img/img3.jpg'
+import img1 from '../../assets/img/testimonial-bg.png'
+import img2 from '../../assets/img/bg-section.png'
+import img3 from '../../assets/img/bg-section-2.png'
 import img4 from '../../assets/img/img4.jpg'
 import img5 from '../../assets/img/img5.jpg'
 import './Index.css'
@@ -9,32 +9,18 @@ import './Index.css'
 export default function Index() {
     return (
         <>
-            <div className="slider">
+            <div className="slider overflow-hidden">
                 <div className="container">
                     <p className="text-center">Trusted by Agencies & Store Owners</p>
-                    <section id="slider">
-                        <input type="radio" name="slider" id="s1" />
-                        <input type="radio" name="slider" id="s2" />
-                        <input type="radio" name="slider" id="s3" checked />
-                        <input type="radio" name="slider" id="s4" />
-                        <input type="radio" name="slider" id="s5" />
-                        <label for="s1" id="slide1">
-                            <img src={img1} height="100%" width="100%" />
-                        </label>
-                        <label for="s2" id="slide2">
-                            <img src={img2} height="100%" width="100%" />
-                        </label>
-                        <label for="s3" id="slide3">
-                            <img src={img3} height="100%" width="100%" />
-                        </label>
-                        <label for="s4" id="slide4">
-                            <img src={img4} height="100%" width="100%" />
-                        </label>
-                        <label for="s5" id="slide5">
-                            <img src={img5} height="100%" width="100%" />
-                        </label>
-                    </section>
-
+                    <div className="bg-img1">
+                        <img src={img2} alt="" />
+                    </div>
+                    <div className="bg-img2">
+                        <img src={img3} alt="" />
+                    </div>
+                </div>
+                <div className="slider-img-box">
+                    <img src={img1} alt="" />
                 </div>
             </div>
         </>
